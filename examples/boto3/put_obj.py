a153395345609a7eb4c6e1b599c355a71bbdf06d
yeop <qkek9352@gmail.com>
2021-03-15 08:45:40 +0000

    new branch

--- examples/boto3/put_obj.py
+++ examples/boto3/put_obj.py
@@ -2,14 +2,28 @@
 
 import boto3
 import json
+import random
+import time
+import numpy as np
+import pickle
+import os
 
 def js_print(arg):
     print(json.dumps(arg, indent=2))
 
+
+def makeRandomBytes(size):
+    random.seed(None)
+    return os.urandom(size)
+
 # endpoint and keys from vstart
-endpoint = 'http://127.0.0.1:8000'
-access_key="0555b35654ad1656d804"
-secret_key="h7GhxuBLTrlhVUyxSPUKUV8r/2EI4ngqJxD7iBdBYLhwluN30JaT3Q=="
+endpoint = 'http://172.31.4.82:80'
+access_key="YG27U5VQH45T5EGC0DD9"
+secret_key="JKQGPJtKV56pTFHZ6JDKGB6ImIgDM1pu9ofnWl4Y"
+writeData = bytes()
+size = 2**20 # 4KB
+num_op = 1
+latencyResults = {}
 
 conn = boto3.resource('s3',
         endpoint_url=endpoint,
@@ -18,10 +32,23 @@ conn = boto3.resource('s3',
 
 
 bucket = conn.Bucket('my-new-bucket')
-bucket.create()
+#bucket.create()
 
-for i in range(100):
+writeData = makeRandomBytes(size)
+
+for i in range(num_op):
+    start = time.perf_counter()
     bucket.put_object(Bucket="my-new-bucket",
-            Key="obj" + str(i),
-            Body="hello",
+            Key="test" + str(i),
+            Body=writeData,
             )
+    end = time.perf_counter() - start
+    latencyResults[str(i)] = str(end)            
+
+print(latencyResults)
+with open('latency', 'ab') as fw:
+    for key, value in latencyResults.items():
+        fw.write(key.encode())
+        fw.write('\t'.encode())
+        fw.write(value.encode())
+        fw.write('\n'.encode())
